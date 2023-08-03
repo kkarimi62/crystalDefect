a7be80ce1ae825dbac2a5c675c972fbf51fe469c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-03 18:39:05 +0200

    update

--- simulations/lmpScripts/kmc.sh
+++ simulations/lmpScripts/kmc.sh
@@ -16,7 +16,7 @@ setenv INI_FILE_NAME             'init_xyz.conf'   # The file name containing th
  
 ###################################### Simulation Details ##################################################
 
-setenv NBRE_KMC_STEPS                10       # The max number of KMC steps to be executed
+setenv NBRE_KMC_STEPS                1       # The max number of KMC steps to be executed
 setenv TOTAL_TIME                    1000.0        # Maximum simulation time in seconds (def: 20 s)
 
 setenv TEMPERATURE                   ${temp} #500.0    # The simulated temperature in kelvin
