a1cbd79392ce3eea7f34500654ed4ce94b89d725
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-08-06 23:17:28 +0200

    update

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -4,7 +4,6 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv):
     confParser = configparser.ConfigParser()
     confParser.read('configuration.ini')
     #--- set parameters
-#	confParser.set('parameters','itime0','5')
     confParser.set('input files','lib_path',os.getcwd()+'/../../HeaDef/postprocess')
     confParser.set('input files','input_path',argv)
     #--- write
@@ -13,11 +12,10 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv):
 
     someFile = open( 'oarScript.sh', 'w' )
     print('#!/bin/bash\n',file=someFile)
-    print('EXEC_DIR=%s\n'%( EXEC_DIR ),file=someFile)    
-    print('module load python/anaconda3-2018.12\nsource /global/software/anaconda/anaconda3-2018.12/etc/profile.d/conda.sh\nconda activate gnnEnv2nd\n\n',file=someFile)
-#	print >> someFile, 'papermill --prepare-only %s/%s ./output.ipynb %s %s'%(EXEC_DIR,PYFIL,argv,argv2nd) #--- write notebook with a list of passed params
+    print('EXEC_DIR=%s\n source /mnt/opt/spack-0.17/share/spack/setup-env.sh\n\nspack load python@3.8.12%%gcc@8.3.0\n\n'%( EXEC_DIR ),file=someFile)
     if convert_to_py:
         print('ipython3 py_script.py\n',file=someFile)
+
     else:
         print('jupyter nbconvert --execute $EXEC_DIR/%s --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(PYFIL), file=someFile)
     someFile.close()										  
@@ -25,46 +23,48 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv):
 if __name__ == '__main__':
     import os
 #
-    runs	 = range(3) #8)
+    runs	 = range(3)
+    nNode    = 1
+    nThreads = 1
     jobname  = {
-                '3':'niNatom1KTemp1000K', 
-                }['3']
+                '4':'nicocrNatom1K/md/temp5', 
+                }['4']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
-                                '3':'/../simulations/niNatom1KTemp1000K',
-                            }['3'] #--- source
+                                '4':'/../simulations/nicocrNatom1K/md/temp5',
+                            }['4'] #--- source
     EXEC_DIR = '.'     #--- path for executable file
     durtn = '23:59:59'
-    mem = '256gb'
-    partition = ['parallel','cpu2019','bigmem','single'][2] 
+    mem = '32gb'
+    partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][1]
     argv = "%s"%(readPath) #--- don't change! 
     PYFILdic = { 
+        0:'postproc.ipynb',
         1:'test.ipynb',
         }
-    keyno = 1 #change!!!!!!!!!
+    keyno = 1
     convert_to_py = True
 #---
 #---
-    PYFIL = PYFILdic[ keyno ] 
-    #--- update argV
+    PYFIL = PYFILdic[ keyno ]
     if convert_to_py:
         os.system('jupyter nbconvert --to script %s --output py_script\n'%PYFIL)
         PYFIL = 'py_script.py'
+    #--- update argV
     #---
     if DeleteExistingFolder:
+        print('rm %s'%jobname)
         os.system( 'rm -rf %s' % jobname ) # --- rm existing
     # --- loop for submitting multiple jobs
     for counter in runs:
         print(' i = %s' % counter)
         writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
         os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
-        os.system( 'cp configuration.ini %s' % ( writPath ) ) #--- cp python module
+#		os.system( 'cp utility.py LammpsPostProcess2nd.py OvitosCna.py %s' % ( writPath ) ) #--- cp python module
         makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s"%counter) # --- make oar script
-        os.system( 'chmod +x oarScript.sh; cp oarScript.sh configuration.ini %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
-        jobname0 = jobname.split('/')[0] #--- remove slash
+        os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s; cp configuration.ini %s;cp %s/%s %s' % ( writPath, writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
         os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
-                            --chdir %s -c %s -n %s %s/oarScript.sh'\
-                           % ( partition, mem, durtn, jobname0, counter, jobname0, counter, jobname0, counter \
-                               , writPath, 1, 1, writPath ) ) # --- runs oarScript.sh!
-
+                                 --chdir %s --ntasks-per-node=%s --nodes=%s %s/oarScript.sh >> jobID.txt'\
+                            % ( partition, mem, durtn, jobname.split('/')[0], counter, jobname.split('/')[0], counter, jobname.split('/')[0], counter \
+                                , writPath, nThreads, nNode, writPath ) ) # --- runs oarScript.sh! 
 
