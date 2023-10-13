1ba44cfe3a0fbd05e71a98bc7aeea94b2deba36f
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-10-13 14:51:34 +0200

    update

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     nThreads = 1
     jobname  = {
                 '4':'niNatom1KTemp1000K3rd', 
-                '5':'neuralNet/niNatom1KTemp1000K3rd', 
+                '5':'neuralNet/niNatom1KTemp1000K4th', 
                 }['5']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
