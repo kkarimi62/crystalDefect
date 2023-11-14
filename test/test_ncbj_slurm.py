ea82e1cd093a6028ab1bd79f57f3aacfc15c5456
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-11-14 16:58:24 +0100

    update

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -24,18 +24,18 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argvv):
 if __name__ == '__main__':
     import os
 #
-    runs	 = range(32) #32)
+    runs	 = range(3) #32)
     nNode    = 1
     nThreads = 1
     jobname  = {
-                '4':'descriptors/ni/10x10', 
+                '4':'descriptors/ni/20x20', 
                 '5':'neuralNet/ni/keras/20x20/cnn', 
-                }['4']
+                }['5']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
                                 '4':'/../simulations/niNatom1KTemp1000K3rd',
                                 '5':'/descriptors/ni/20x20',
-                            }['4'] #--- source
+                            }['5'] #--- source
     EXEC_DIR = '.'     #--- path for executable file
     durtn = '23:59:59'
     mem = '64gb'
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         0:'buildDescriptors.ipynb',
         1:'neuralNetwork.ipynb',
         }
-    keyno = 0
+    keyno = 1
     convert_to_py = True
 #---
 #---
