e6742cf09539c0de4c1e804ef8574498cbe0d471
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-11-22 22:56:24 +0100

    update

--- test/submitPY.py
+++ test/submitPY.py
@@ -5,10 +5,11 @@ if __name__ == '__main__':
     #---
     lnums = [ 32, 8, 9, 10 ]
     script = 'postproc.py test_ncbj_slurm.py'.split()[1]
-    number_hidden_layers  = dict(zip(range(4),[1,2,3]))
-    n_channels  = dict(zip(range(4),[8,16,32,64]))
+    number_hidden_layers  = dict(zip(range(4),[1,2,3,4]))
+    hidden_layer_size     = dict(zip(range(4),[10,100,1000,10000]))
+#     n_channels  = dict(zip(range(4),[8,16,32,64]))
 #     activations = dict(zip(range(20),['linear','sigmoid','relu','softmax','softplus','softsign','tanh','selu','elu','exponential']))
-    activations = dict(zip(range(20),['linear','relu']))
+    activations = dict(zip(range(20),['linear']))
 
     string=open(script).readlines() #--- python script
     #---
@@ -18,17 +19,17 @@ if __name__ == '__main__':
     count = 0
     for key_n in number_hidden_layers:
         number_hidden_layer = number_hidden_layers[key_n]
-        for key_c in n_channels:
-            n_channel = n_channels[key_c]
+        for key_c in hidden_layer_size:
+            n_channel = hidden_layer_size[key_c]
             for key_a in activations:
                 activation = activations[key_a]
 
         #---	
                 inums = lnums[ 0 ] - 1
-                string[ inums ] = "\t\'5\':\'neuralNet/ni/keras/20x20/cnn/classification3rd/layer%s/channel%s/activation%s\',\n" % (key_n,key_c,key_a) #--- change job name
+                string[ inums ] = "\t\'5\':\'neuralNet/ni/keras/20x20/ann/classifier/layer%s/channel%s/activation%s\',\n" % (key_n,key_c,key_a) #--- change job name
         #---	densities
                 inums = lnums[ 1 ] - 1
-                string[ inums ] = "    confParser.set(\'neural net\',\'n_channels\',\'%s\')\n"%(n_channel)
+                string[ inums ] = "    confParser.set(\'neural net\',\'hidden_layer_size\',\'%s\')\n"%(n_channel)
                 #
                 inums = lnums[ 2 ] - 1
                 string[ inums ] = "    confParser.set(\'neural net\',\'number_hidden_layers\',\'%s\')\n"%(number_hidden_layer)
