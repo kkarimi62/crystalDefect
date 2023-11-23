e87ed4bfa2d6e6ef3cd7ddc814477e0e39f1fe6c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-11-23 11:30:23 +0100

    update

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -5,10 +5,10 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argvv):
     confParser.set('input files','lib_path', '%s %s'%(os.getcwd()+'/../../HeaDef/postprocess',os.getcwd()))
     confParser.set('input files','input_path',argv)
     confParser.set('neural net','input_path',argvv)
-    confParser.set('neural net','n_channels','1')
-    confParser.set('neural net','number_hidden_layers','1')
-    confParser.set('neural net','activation',"'linear'")
-    confParser.set('neural net','hidden_layer_size','100')
+#     confParser.set('neural net','n_channels','1')
+#     confParser.set('neural net','number_hidden_layers','1')
+#     confParser.set('neural net','activation',"'linear'")
+#     confParser.set('neural net','hidden_layer_size','100')
     #--- write
     confParser.write(open('configuration.ini','w'))	
     #--- set environment variables
