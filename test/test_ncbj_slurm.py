7a9ebc40dd60cd295c1b47c327d0ad881d871598
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-12-01 08:51:41 +0100

    update

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -33,19 +33,19 @@ if __name__ == '__main__':
                 '4':'descriptors/ni/20x20', 
                 '5':'test',#'neuralNet/20x20/ml', 
                 '6':'mlmc', 
-                }['6']
+                }['5']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
                                 '4':'/../simulations/niNatom1KTemp1000K3rd',
                                 '5':'/descriptors/ni/20x20',
                                 '6':'/test', #'/neuralNet/20x20/ml', 
-                            }['6'] #--- source
+                            }['5'] #--- source
     PYFILdic = { 
         0:'buildDescriptors.ipynb',
         1:'neuralNetwork.ipynb',
         2:'mlmc.ipynb',
         }
-    keyno = 2
+    keyno = 1
     EXEC_DIR = '.'     #--- path for executable file
     durtn = '23:59:59'
     mem = '64gb'
