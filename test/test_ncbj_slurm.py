95f8ca912870d074d422c221ce7e37c682317a63
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-12-18 09:42:30 +0100

    update

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -33,20 +33,20 @@ if __name__ == '__main__':
     jobname  = {
                 '4':'descriptors/ni/void5th', 
                 '5':'neuralNet/ni/new_void5th', 
-                '6':'mlmc', 
-                }['5']
+                '6':'mlmc/ni/new_void5th', 
+                }['6']
     DeleteExistingFolder = True
     readPath = os.getcwd() + {
                                 '4':'/../simulations/ni/void5th',
                                 '5':'/descriptors/ni/void5th',
                                 '6':'/neuralNet/ni/new_void5th', 
-                            }['5'] #--- source
+                            }['6'] #--- source
     PYFILdic = { 
         0:'buildDescriptors.ipynb',
         1:'neuralNetwork.ipynb',
         2:'mlmc.ipynb',
         }
-    keyno = 1
+    keyno = 2
     EXEC_DIR = '.'     #--- path for executable file
     durtn = '23:59:59'
     mem = '128gb'
