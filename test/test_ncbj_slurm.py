f44357b9ff63e36e28d337d825692a230a1e72bf
kkarimi62 <kamran.karimi1362@gmail.com>
2023-12-20 13:13:25 +0100

    updates

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -1,7 +1,7 @@
 #from backports 
 import configparser
 def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argvv):
-    confParser = configparser.ConfigParser()
+    confParser           = configparser.ConfigParser()
     confParser.read('configuration.ini')
     confParser.set('input files','lib_path', '%s %s'%(os.getcwd()+'/../../HeaDef/postprocess',os.getcwd()))
     confParser.set('input files','input_path',argv)
@@ -11,7 +11,7 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argvv):
     confParser.write(open('configuration.ini','w'))	
     #--- set environment variables
 
-    someFile = open( 'oarScript.sh', 'w' )
+    someFile             = open( 'oarScript.sh', 'w' )
     print('#!/bin/bash\n',file=someFile)
     print('EXEC_DIR=%s\n source /mnt/opt/spack-0.17/share/spack/setup-env.sh\n\nspack load python@3.8.12%%gcc@8.3.0\n\n'%( EXEC_DIR ),file=someFile)
     if convert_to_py:
@@ -23,41 +23,41 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argvv):
 if __name__ == '__main__':
     import os
 #
-    runs	 = range(32)
-    nNode    = 1
-    nThreads = 1
-    jobname  = {
-                '4':'descriptors/ni/void5th', 
-                '5':'neuralNet/ni/new_void5th', 
-                '6':'mlmc/ni/latest_void5th', 
-                }['6']
+    runs	             = range( 8 )
+    nNode                = 1
+    nThreads             = 1
+    jobname              = {
+                            '4':'descriptors/ni/void_2d', 
+                            '5':'neuralNet/ni/new_void5th', 
+                            '6':'mlmc/ni/latest_void5th', 
+                            }['4']
     DeleteExistingFolder = True
-    readPath = os.getcwd() + {
-                                '4':'/../simulations/ni/void5th',
-                                '5':'/descriptors/ni/void5th',
-                                '6':'/neuralNet/ni/new_void5th', 
-                            }['6'] #--- source
-    PYFILdic = { 
-        0:'buildDescriptors.ipynb',
-        1:'neuralNetwork.ipynb',
-        2:'mlmc.ipynb',
-        }
-    keyno = 2
-    EXEC_DIR = '.'     #--- path for executable file
-    durtn = '23:59:59'
-    mem = '128gb'
-    partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL','GPU_K80'][ 2 ]
-    argv = "%s"%(readPath) #--- don't change! 
-    convert_to_py = True
+    readPath             = os.getcwd() + {
+                                            '4':'/../simulations/ni/void_2d
+                                            '5':'/descriptors/ni/void5th',
+                                            '6':'/neuralNet/ni/new_void5th', 
+                                        }['4'] #--- source
+    PYFILdic             = { 
+                            0:'buildDescriptors.ipynb',
+                            1:'neuralNetwork.ipynb',
+                            2:'mlmc.ipynb',
+                            }
+    keyno                = 0
+    EXEC_DIR             = '.'     #--- path for executable file
+    durtn                = '23:59:59'
+    mem                  = '128gb'
+    partition            = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL','GPU_K80'][ 2 ]
+    argv                 = "%s"%(readPath) #--- don't change! 
+    convert_to_py        = True
 #---
 #---
-    additional_args = ''
-    if partition == 'GPU_K80':
-        additional_args = '--gres=gpu:tesla:1'
-    PYFIL = PYFILdic[ keyno ]
+    additional_args      = ''
+    if partition         == 'GPU_K80':
+        additional_args  = '--gres=gpu:tesla:1'
+    PYFIL                = PYFILdic[ keyno ]
     if convert_to_py:
         os.system('jupyter nbconvert --to script %s --output py_script\n'%PYFIL)
-        PYFIL = 'py_script.py'
+        PYFIL            = 'py_script.py'
     #--- update argV
     #---
     if DeleteExistingFolder:
@@ -66,13 +66,13 @@ if __name__ == '__main__':
     # --- loop for submitting multiple jobs
     for counter in runs:
         print(' i = %s' % counter)
-        writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
+        writPath         = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
         os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
         for keys in PYFILdic:
             os.system( 'ln -s %s/%s %s/%s' % ( os.getcwd(),PYFILdic[keys],writPath,PYFILdic[keys] ) ) #--- cp python modules
         makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s"%counter, argv) # --- make oar script
         os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s; cp configuration.ini %s;cp %s/%s %s' % ( writPath, writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
-        jobname0 = jobname.replace('/','_')
+        jobname0         = jobname.replace('/','_')
         os.system( 'sbatch --partition=%s --mem=%s --time=%s %s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
                                  --chdir %s --ntasks-per-node=%s --nodes=%s --export=slurm_path=%s %s/oarScript.sh >> jobID.txt'\
                             % ( partition, mem, durtn, additional_args, jobname0, counter, jobname0, counter, jobname0, counter \
