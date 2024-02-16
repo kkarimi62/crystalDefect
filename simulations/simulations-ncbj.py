28c07a83d59dc13456fa2a59bd3aa519d21d5abe
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2024-02-15 16:16:38 -0800

    update

--- simulations/simulations-ncbj.py
+++ simulations/simulations-ncbj.py
@@ -26,14 +26,15 @@ if __name__ == '__main__':
 
         nruns	 = 32
         #
-        nThreads = 4
+        nThreads = 8 #4
         nNode	 = 1
         #
         jobname  = {
                     4:'ni/niNatom1KTemp300K', 
+                    5:'ni/void_2d_training', 
                     6:'ni/pure', 
-                    5:'ni/void_2d_training_closer', 
-                   }[5]
+                    7:'ni/dislocation', 
+                   }[7]
         sourcePath = os.getcwd() +\
                     {	
                         0:'/junk',
@@ -93,7 +94,7 @@ if __name__ == '__main__':
                     6:' -var T 300 -var DataFile Equilibrated_300.dat',
                     4:' -var T 600.0 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 100 -var ParseData 1 -var WriteData swapped_600.dat', 
                     5:' -var buff 0.0 -var nevery 1000 -var ParseData 0 -var lx 20 -var ly 20 -var lz 3 -var ntype 2 -var cutoff 3.54  -var DumpFile dumpMin.xyz -var WriteData data_pure.dat -var seed0 %s -var seed1 %s -var seed2 %s -var seed3 %s'%tuple(np.random.randint(1001,9999,size=4)), 
-                    51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile dataVoidVac.dat -var DumpFile dumpMin.xyz -var WriteData data_minimized.dat', 
+                    51:' -var buff 0.0 -var nevery 1000 -var ParseData 1 -var DataFile data_dislocated_void.dat -var DumpFile dumpMin.xyz -var WriteData data_minimized.dat', 
                     7:' -var buff 0.0 -var T 300.0 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile data_minimized.dat -var DumpFile dumpThermalized.xyz -var WriteData data_thermalized.dat -var rnd %s'%np.random.randint(1001,9999),
                     71:' -var buff 0.0 -var T 0.1 -var P 0.0 -var nevery 100 -var ParseData 1 -var DataFile swapped_600.dat -var DumpFile dumpThermalized2.xyz -var WriteData Equilibrated_0.dat',
                     8:' -var buff 0.0 -var T 0.1 -var sigm 1.0 -var sigmdt 0.0001 -var ndump 100 -var ParseData 1 -var DataFile Equilibrated_0.dat -var DumpFile dumpSheared.xyz',
@@ -101,9 +102,9 @@ if __name__ == '__main__':
                     10:' -var ParseData 1 -var DataFile swapped_600.dat',
                     'p0':' swapped_600.dat 10.0 %s'%(os.getcwd()+'/../postprocess'),
                     'p1':' swapped_600.dat ElasticConst.txt DumpFileModu.xyz %s'%(os.getcwd()+'/../postprocess'),
-                    'p2':' %s 3.52 135.0 67.0 135.0 data.txt 5'%(os.getcwd()+'/../postprocess'),
+                    'p2':' %s 3.52 135.0 67.0 135.0 data_dislocated.txt 5'%(os.getcwd()+'/../postprocess'),
                     'p3':' data_minimized.dat init_xyz.conf %s 300.0'%(os.getcwd()+'/lmpScripts'),
-                    'p4':' data_void.dat dataVoidVac.dat %s 1 1 35.0'%(os.getcwd()+'/lmpScripts'),
+                    'p4':' data_dislocated.dat data_dislocated_void.dat %s 1 1 48.0'%(os.getcwd()+'/lmpScripts'),
 #                    'p4':' data_pure.dat dataVoidVac.dat %s 1 1 48.0'%(os.getcwd()+'/lmpScripts'),
                     'p5':' ',
                     'p6':' data_pure.dat data_void.dat %s 4.0 2'%(os.getcwd()+'/lmpScripts'),
@@ -129,15 +130,16 @@ if __name__ == '__main__':
                     12:[ 'p5',1.0], #--- restart 11: set restart = True in kmc.sh
                     13:[5, 'p4', 51, 'p3','p5',1.0], #--- min.,add vacancy,min.,kmc input,kart.sh to bash ,invoke kart
                     11:[5,'p6', 'p7', 'p4', 51, 'p3','p5',1.0], #--- min.,add void,add subgroup,add vacancy,min.,kmc input,kart.sh to bash ,invoke kart
-                  }[11]
+                    14:['p2', 'p4', 51, 'p3','p5',1.0], #--- put disc. by atomsk, add vacancy, minimize, kmc input,kart.sh to bash ,invoke kart
+                  }[14]
         Pipeline = list(map(lambda x:LmpScript[x],indices))
     #	Variables = list(map(lambda x:Variable[x], indices))
     #        print('EXEC=',EXEC)
         #
         EXEC_lmp = ['lmp_g++_openmpi'][0]
-        durtn = ['23:59:59','167:59:59'][ 0 ]
+        durtn = ['47:59:59','167:59:59'][ 0 ]
         mem = '16gb' #'22gb'
-        partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][1]
+        partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][3]
         #--
         DeleteExistingFolder = True
 
