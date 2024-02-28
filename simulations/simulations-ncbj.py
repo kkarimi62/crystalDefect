46f5e0c9b4ebbfcfdbe3a085a1b861d4d983e8cd
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2024-02-28 10:00:56 -0800

    irradiation implemented!

--- simulations/simulations-ncbj.py
+++ simulations/simulations-ncbj.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
     #        print('EXEC=',EXEC)
         #
         EXEC_lmp = ['lmp_g++_openmpi'][0]
-        durtn = ['47:59:59','167:59:59'][ 0 ]
+        durtn = ['23:59:59','47:59:59','167:59:59'][ 0 ]
         mem = '16gb' #'22gb'
         partition = ['INTEL_PHI','INTEL_CASCADE','INTEL_SKYLAKE','INTEL_IVY','INTEL_HASWELL'][0]
         #--
