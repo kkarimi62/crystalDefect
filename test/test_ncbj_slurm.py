4a8fe2ef74911c6067f940f3ffad378c9e563ac1
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2024-03-12 16:45:11 -0700

    irradiation implemented

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -29,20 +29,20 @@ if __name__ == '__main__':
     jobname              = {
                             '4':'descriptors/ni/pure/test', 
                             '5':'neuralNet/ni/pure/test', 
-                            '6':'mlmc/ni/void_2d_training', 
-                            }['5']
+                            '6':'mlmc/ni/pure/test', 
+                            }['6']
     DeleteExistingFolder = True
     readPath             = os.getcwd() + {
                                             '4':'/../simulations/ni/pure/test',
                                             '5':'/descriptors/ni/pure/test',
-                                            '6':'/neuralNet/ni/void_2d_training', 
-                                        }['5'] #--- source
+                                            '6':'/neuralNet/ni/pure/test', 
+                                        }['6'] #--- source
     PYFILdic             = { 
                             0:'buildDescriptors.ipynb',
                             1:'neuralNetwork.ipynb',
                             2:'mlmc.ipynb',
                             }
-    keyno                = 1
+    keyno                = 2
     EXEC_DIR             = '.'     #--- path for executable file
     durtn                = '23:59:59'
     mem                  = '16gb'
