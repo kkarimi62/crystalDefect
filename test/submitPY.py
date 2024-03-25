c5fba117b8a424e32dea96cfb1871dfcfccc912e
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2024-03-25 05:40:19 -0700

    updates

--- test/submitPY.py
+++ test/submitPY.py
@@ -16,22 +16,22 @@ if __name__ == '__main__':
     count = 0
     for key_n in number_hidden_layers:
         number_hidden_layer = number_hidden_layers[key_n]
-                for key_h in hidden_layer_size:
-                    nsize = hidden_layer_size[key_h]
+        for key_h in hidden_layer_size:
+            nsize = hidden_layer_size[key_h]
 
-        #---	
-                    inums = lnums[ 0 ] - 1
-                    string[ inums ] = "\t\'5\':\'neuralNet/ni/pure/test/layer%s/layer_size%s\',\n" % (key_n,key_h) #--- change job name
-            #---	densities
-                    #
-                    inums = lnums[ 1 ] - 1
-                    string[ inums ] = "    confParser.set(\'gnn\',\'num_layers\',\'%s\')\n"%(number_hidden_layer)
-                    #
-                    #
-                    inums = lnums[ 2 ] - 1
-                    string[ inums ] = "    confParser.set(\'gnn\',\'c_hidden\',\'%s\')\n"%(nsize)
-                    #
-                    sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
-                    os.system( 'python junk%s.py'%count )
-                    os.system( 'rm junk%s.py'%count )
-                    count += 1
+#---	
+            inums = lnums[ 0 ] - 1
+            string[ inums ] = "\t\'5\':\'neuralNet/ni/pure/test/layer%s/layer_size%s\',\n" % (key_n,key_h) #--- change job name
+    #---	densities
+            #
+            inums = lnums[ 1 ] - 1
+            string[ inums ] = "    confParser.set(\'gnn\',\'num_layers\',\'%s\')\n"%(number_hidden_layer)
+            #
+            #
+            inums = lnums[ 2 ] - 1
+            string[ inums ] = "    confParser.set(\'gnn\',\'c_hidden\',\'%s\')\n"%(nsize)
+            #
+            sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
+            os.system( 'python junk%s.py'%count )
+            os.system( 'rm junk%s.py'%count )
+            count += 1
