27b210218553ffe38228f8bfc8e1a8583bc45fa2
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2024-03-24 22:02:02 -0700

    updates

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -25,17 +25,17 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argvv):
 if __name__ == '__main__':
     import os
 #
-    runs	             = range( 1 )
+    runs	             = range( 3 )
     nNode                = 1
     nThreads             = 1
     jobname              = {
-                            '4':'descriptors/ni/interestitials/test2nd',#'descriptors/ni/pure/test', 
+                            '4':'descriptors/ni/pure/test2nd',#'descriptors/ni/pure/test', 
                             '5':'neuralNet/ni/pure/test4th',#'neuralNet/ni/pure/test', 
                             '6':'mlmc/ni/interestitials/test2nd', 
-                            }['5']
+                            }['4']
     DeleteExistingFolder = True
     readPath             = os.getcwd() + {
-                                            '4':'/../simulations/ni/pure/test',
+                                            '4':'/../simulations/ni/pure/test2nd',
                                             '5':'/descriptors/ni/interestitials/test2nd',#'/descriptors/ni/pure/test',
                                             '6':'/neuralNet/ni/interestitials/test2nd', 
                                         }['4'] #--- source
@@ -44,7 +44,7 @@ if __name__ == '__main__':
                             1:'neuralNetwork.ipynb',
                             2:'mlmc.ipynb',
                             }
-    keyno                = 1
+    keyno                = 0
     EXEC_DIR             = '.'     #--- path for executable file
     durtn                = '23:59:59'
     mem                  = '16gb'
