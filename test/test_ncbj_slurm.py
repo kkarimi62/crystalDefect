1fa9f3b30c1083c16b60ca9eb2c1996f5b7980fc
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2024-04-04 17:39:32 -0700

    updates

--- test/test_ncbj_slurm.py
+++ test/test_ncbj_slurm.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     nThreads             = 1
     jobname              = {
                             '4':'descriptors/ni/pure/bulk_testset_100',#'descriptors/ni/pure/test', 
-                            '5':'neuralNet/ni/pure/edgeAttrs_overfit2nd_Dropout_testset_100',#shape10x10x10_overfitting_trainSize',#'neuralNet/ni/pure/test', 
+                            '5':'neuralNet/ni/pure/overfit_nodal_features',#shape10x10x10_overfitting_trainSize',#'neuralNet/ni/pure/test', 
                             '6':'mlmc/ni/interestitials/test2nd', 
                             }['5']
     DeleteExistingFolder = True
